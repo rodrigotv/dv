72cd5e71362de4a40692bb7b669acb202e31d8de
Rodrigo Trigosso <rodrigotv@yahoo.com>
2024-03-16 13:59:37 -0400

    Almost ready. Still pending background images and contact forms

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/#home-about', title: 'About' },
+  { href: '/about', title: 'About' },
   { href: '/#home-product-development', title: 'Product Design' },
   { href: '/#home-business-development', title: 'Retail Business Development' },
   { href: '/#home-training', title: 'Training Programs' },
