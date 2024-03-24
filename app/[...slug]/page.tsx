3c09e7dad13cd2dd8a4d51a692651f5f5d2162dc
Rodrigo Trigosso <rodrigotv@yahoo.com>
2024-03-23 21:27:43 -0400

    favicon, text color ready; titles, forms and animations pending

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -2,55 +2,55 @@ import { Pages, allPages } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import PageLayout from '@/layouts/PageLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
-// import { genPageMetadata } from 'app/seo'
-import { Metadata } from 'next'
+import { genPageMetadata } from 'app/seo'
+// import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { components } from '@/components/MDXComponents' 
 
-import siteMetadata from '@/data/siteMetadata'
-// export const metadata = genPageMetadata({ title: ''})
+// import siteMetadata from '@/data/siteMetadata'
+export const metadata = genPageMetadata({ title: ''})
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string[] }
-}): Promise<Metadata | undefined> {
-  const slug = decodeURI(params.slug.join('/'))
-  const page = allPages.find((p) => p.slug === slug)
-  if (!page) {
-    return
-  }
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { slug: string[] }
+// }): Promise<Metadata | undefined> {
+//   const slug = decodeURI(params.slug.join('/'))
+//   const page = allPages.find((p) => p.slug === slug)
+//   if (!page) {
+//     return
+//   }
 
-  let imageList = [siteMetadata.socialBanner]
-  if (page.images) {
-    imageList = typeof page.images === 'string' ? [page.images] : page.images
-  }
-  const ogImages = imageList.map((img) => {
-    return {
-      url: img.includes('http') ? img : siteMetadata.siteUrl + img,
-    }
-  })
+//   let imageList = [siteMetadata.socialBanner]
+//   if (page.images) {
+//     imageList = typeof page.images === 'string' ? [page.images] : page.images
+//   }
+//   const ogImages = imageList.map((img) => {
+//     return {
+//       url: img.includes('http') ? img : siteMetadata.siteUrl + img,
+//     }
+//   })
 
-  return {
-    title: page.title,
-    description: page.summary,
-    openGraph: {
-      title: page.title,
-      description: page.summary,
-      siteName: siteMetadata.title,
-      locale: 'en_US',
-      type: 'article',
-      url: './',
-      images: ogImages,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: page.title,
-      description: page.summary,
-      images: imageList,
-    },
-  }
-}
+//   return {
+//     title: page.title,
+//     description: page.summary,
+//     openGraph: {
+//       title: page.title,
+//       description: page.summary,
+//       siteName: siteMetadata.title,
+//       locale: 'en_US',
+//       type: 'article',
+//       url: './',
+//       images: ogImages,
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: page.title,
+//       description: page.summary,
+//       images: imageList,
+//     },
+//   }
+// }
 
 export default function Page({ params }: { params: { slug: string[] } }) {
   const slug = decodeURI(params.slug.join('/'))
