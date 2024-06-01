15f780526e15f060bd53a30e2283b638bc0bac25
Rodrigo Trigosso <rodrigotv@yahoo.com>
2024-06-01 04:41:24 -0400

    agregadas fotos con créditos

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -8,6 +8,11 @@ interface Props {
 export default function SectionContainer({ children, id }: Props) {
 
   return (
-    <section id={id} className="section-container w-full m-0 p-0">{children}</section>
+    <section 
+      id={id} 
+      className="section-container w-full m-0 p-0" 
+    >
+      {children}
+    </section>
   )
 }
